0a4f426e409ed38461f6e35224e87ec54328e1e2
Ryan Martin <ryan@vestahealthcare.com>
2022-12-16 22:34:45 -0500

    npx projen upgrade

--- .projenrc.ts
+++ .projenrc.ts
@@ -11,19 +11,9 @@ const project = new SkepPluginProject({
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
   majorVersion,
-  releaseBranches: {
-    dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
-  },
-  depsUpgradeOptions: {
-    workflowOptions: {
-      branches: ['main'],
-    },
-  },
-  entrypoint: 'dist/index.js',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.compileTask.exec('tsc --declaration');
 project.synth();
